ade885022b86c25469dbf50839e92eb269cd91e7
user <user@gmail.com>
2023-04-27 18:58:07 +0300

    Toggle env

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,17 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const assert = chai.assert
+const server = require('../server')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
-suite('Functional Tests', function() {
-
-});
+/*
+Viewing one stock: GET request to /api/stock-prices/
+Viewing one stock and liking it: GET request to /api/stock-prices/
+Viewing the same stock and liking it again: GET request to /api/stock-prices/
+Viewing two stocks: GET request to /api/stock-prices/
+Viewing two stocks and liking them: GET request to /api/stock-prices/
+*/
+suite('Functional Tests', function () {
+  
+})
