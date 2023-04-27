142a2fca42162caa262f4c54a31c5a31d5281b87
user <user@gmail.com>
2023-04-27 15:18:33 +0300

    Add async/await fetch request

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,12 @@
 'use strict'
 
 module.exports = function (app) {
-  app.route('/api/stock-prices').get(function (req, res) {
-    res.json('ok')
-  })
+  app.route('/api/stock-prices').get(
+    // Use async function allow to use await inside
+    async (req, res) => {
+      let rawData = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock}/quote`)
+      let data = await rawData.json()
+      res.json({ stockData: { stock: data.symbol, price: data.latestPrice, likes: 1 } })
+    }
+  )
 }
