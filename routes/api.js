0a685077c276709d51a0d4fa8acb4a540cc47024
user <user@gmail.com>
2023-04-27 11:24:13 +0300

    Add nodemon, refactor

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,7 @@
-'use strict';
+'use strict'
 
 module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
-};
+  app.route('/api/stock-prices').get(function (req, res) {
+    res.json('ok')
+  })
+}
