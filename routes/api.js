562b1dab302ca0e6fa10638aa405943d84697990
user <user@gmail.com>
2023-04-29 13:20:21 +0300

    WIP 'like' system add ips array

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,19 @@
 'use strict'
 // Initialize variables
 let stockLikes = {}
+/* stockLikes = {
+  goog:
+    {
+      likes :2,
+      ips: [111, 123, 222]
+    },
+  msft:
+    {
+      likes: 1,
+      ips: [111, 123, 222]
+    }
+  }
+*/
 
 module.exports = function (app) {
   app.route('/api/stock-prices').get(
@@ -10,24 +23,27 @@ module.exports = function (app) {
       if (typeof req.query.stock === 'string') {
         // 'likes' system
         if (stockLikes.hasOwnProperty(req.query.stock)) {
-          if (req.query.like === 'true') {
-            stockLikes[req.query.stock]++
+          if (req.query.like === 'true' && !stockLikes[req.query.stock]['ips'].includes(req.ip)) {
+            stockLikes[req.query.stock]['likes']++
+            stockLikes[req.query.stock]['ips'].push(req.ip)
           }
         } else {
-          stockLikes[req.query.stock] = 0
+          stockLikes[req.query.stock] = {
+            likes: 0,
+            ips: [],
+          }
         }
+        console.log(stockLikes)
 
-        let rawData = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock}/quote`)
-        let data = await rawData.json()
-        res.json({ stockData: { stock: data.symbol, price: data.latestPrice, likes: stockLikes[req.query.stock] } })
+        let data = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock}/quote`).then((rawData) => rawData.json())
+        res.json({ stockData: { stock: data.symbol, price: data.latestPrice, likes: stockLikes[req.query.stock]['likes'] } })
+        // res.json({ stockData: { stock: data.symbol, price: data.latestPrice, likes: 0 } })
 
         // Return {"stockData":[{"stock":"MSFT","price":295.37,"rel_likes":820},{"stock":"GOOG","price":104.45,"rel_likes":-820}]}
         // Check if multiple query parameters with the same name (/api/stock-prices?stock=goog&stock=msft), req.query[paramName] will return an array
       } else if (Array.isArray(req.query.stock)) {
-        let rawData0 = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock[0]}/quote`)
-        let rawData1 = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock[1]}/quote`)
-        let data0 = await rawData0.json()
-        let data1 = await rawData1.json()
+        let data0 = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock[0]}/quote`).then((rawData) => rawData.json())
+        let data1 = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${req.query.stock[1]}/quote`).then((rawData) => rawData.json())
         res.json({
           stockData: [
             { stock: data0.symbol, price: data0.latestPrice, rel_likes: stockLikes[req.query.stock[0]] },
